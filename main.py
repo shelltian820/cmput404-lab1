eb3f52393716550080e41c73fa04382721d27cc2
shelley <stian3@ualberta.ca>
2019-01-10 21:12:16 -0500

    print stuff

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import requests
 print(requests.__version__)
 r = requests.get("https://raw.githubusercontent.com/shelltian820/cmput404-lab1/master/main.py")
 # print(dir(r))
-# print(r.text)
+print(r.text)
 # print(r.status_code)
